5d1bcc71d9970e7074cddc03bd127d9eec512441
wlmiller <wlmiller@gmail.com>
2014-02-10 09:18:46 -0600

    added finding the shortest path length

--- astar.hs
+++ astar.hs
@@ -1,24 +1,60 @@
 import System.IO
+import Data.List
+import Data.Ord
 
-type AStarMap = [[(Char,Int)]]
+type PosList = [((Int, Int), (Float, Float))]
 
 main = do
 	handle <- openFile "map.txt" ReadMode
 	inputMap <- hGetContents handle
-	let mapLines = valid . lines $ inputMap
+	let mapLines = validateMap . lines $ inputMap	-- It's my understanding that a list of lists is a poor way to do this.  I'd like to switch this out for a 2D array eventually.
 	let exitPos = findChar '$' mapLines
 	let entryPos = findChar '@' mapLines
+	let heuristic = heuristic' mapLines exitPos
 	putStr $ unlines $ mapLines
 	putStrLn $ "Exit location:  " ++ (show exitPos) 
 	putStrLn $ "Entry location: " ++ (show entryPos)
+	let firststep = findPath heuristic entryPos
+	putStrLn ""
+	putStrLn $ "Shortest path: " ++ (show $ gScore . head . fst $ firststep) ++ " units."
 	hClose handle
 
-valid :: [[Char]] -> [[Char]]	
-valid m 
+gScore (_,(g,_)) = g
+hScore (_,(_,h)) = h
+fScore pos = (gScore pos) + (hScore pos)
+	
+expandFrontier :: ((Int, Int) -> Float) -> (PosList, PosList) -> (PosList, PosList)
+expandFrontier heuristic (visited, frontier) 
+	| hScore lowestHpos == 0 = (lowestHpos:visited, [])
+	| otherwise = expandFrontier heuristic $ expandFrom pos
+	where
+		lowestHpos = head . sortBy (comparing hScore) $ frontier
+		pos = head . sortBy (comparing fScore) $ frontier
+		rest = tail . sortBy (comparing fScore) $ frontier
+		visitedPos = [ fst x | x <- visited ]
+		
+		neighbors (x,y) = 
+			[ ((x+dx, y+dy), (gScore pos + 1.0, heuristic (x+dx, y+dy))) | dx <- [-1..1] 
+																		, dy <- [-1..1] 
+																		, (abs dx) + (abs dy) == 1
+																		, heuristic (x+dx,y+dy) /= read "Infinity"
+																		, not $ (x + dx, y + dy) `elem` visitedPos ]
+		
+		expandFrom pos = (pos:visited, (neighbors $ fst pos) ++ rest)
+ 
+findPath heuristic entryPos = expandFrontier heuristic ([], [(entryPos, (0.0, heuristic entryPos))])
+ 
+heuristic' :: [[Char]] -> (Int, Int) -> (Int, Int) -> Float
+heuristic' m (endX, endY) (x, y)
+	| (m !! y) !! x == '#' = read "Infinity"
+	| otherwise = fromIntegral $ (abs (x - endX)) + (abs (y - endY))
+	
+validateMap :: [[Char]] -> [[Char]]	
+validateMap m 
 	| (length . concat . map (filter (=='$')) $ m) /= 1 = error "Must have exactly one exit ('$')!"
 	| (length . concat . map (filter (=='@')) $ m) /= 1 = error "Must have exactly one entrance ('@')!"
 	| (length . concat . map (filter (not . flip(elem) "#.$@")) $ m) > 0 = error "Unexpected symbol!  Only '#', '.', '$', and '@' allowed."
-	| otherwise = -- Make sure there's a wall around the whole map.  This way there's no need to worry about running off the map later.
+	| otherwise =	-- Make sure there's a wall around the whole map.  This way there's no need to worry about running off the map later.
 		let
 			width = length . head $ m
 			m' = if (length . filter (/= '#') . head $ m) > 0 
@@ -28,15 +64,13 @@ valid m
 				then reverse ((replicate width '#'):(reverse m')) 
 				else m'
 		
-			m''' = 
-				if length [ head xs | xs <- m'', head xs /= '#'] > 0 
-					then [ '#':xs | xs <- m'' ]
-					else m''
+			m''' = if length [ head xs | xs <- m'', head xs /= '#'] > 0 
+				then [ '#':xs | xs <- m'' ]
+				else m''
 					
-			m'''' =
-				if length [ last xs | xs <- m''', last xs /= '#'] > 0 
-					then [ reverse ('#':(reverse xs)) | xs <- m''' ]
-					else m'''
+			m'''' = if length [ last xs | xs <- m''', last xs /= '#'] > 0 
+				then [ reverse ('#':(reverse xs)) | xs <- m''' ]
+				else m'''
 		in m''''
 		
 findChar :: (Eq a) => a -> [[a]] -> (Int, Int)
