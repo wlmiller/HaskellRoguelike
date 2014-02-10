0e782495702fe3f194323d224f9ed34b9127ac97
wlmiller <wlmiller@gmail.com>
2014-02-09 21:03:53 -0600

    get coordinates of entry and exit points

--- astar.hs
+++ astar.hs
@@ -1,10 +1,16 @@
 import System.IO
 
+type AStarMap = [[(Char,Int)]]
+
 main = do
 	handle <- openFile "map.txt" ReadMode
 	inputMap <- hGetContents handle
-	let mapLines = lines inputMap
-	putStrLn $ unlines $ valid mapLines
+	let mapLines = valid . lines $ inputMap
+	let exitPos = findChar '$' mapLines
+	let entryPos = findChar '@' mapLines
+	putStr $ unlines $ mapLines
+	putStrLn $ "Exit location:  " ++ (show exitPos) 
+	putStrLn $ "Entry location: " ++ (show entryPos)
 	hClose handle
 
 valid :: [[Char]] -> [[Char]]	
@@ -12,7 +18,7 @@ valid m
 	| (length . concat . map (filter (=='$')) $ m) /= 1 = error "Must have exactly one exit ('$')!"
 	| (length . concat . map (filter (=='@')) $ m) /= 1 = error "Must have exactly one entrance ('@')!"
 	| (length . concat . map (filter (not . flip(elem) "#.$@")) $ m) > 0 = error "Unexpected symbol!  Only '#', '.', '$', and '@' allowed."
-	| otherwise = 
+	| otherwise = -- Make sure there's a wall around the whole map.  This way there's no need to worry about running off the map later.
 		let
 			width = length . head $ m
 			m' = if (length . filter (/= '#') . head $ m) > 0 
@@ -32,3 +38,13 @@ valid m
 					then [ reverse ('#':(reverse xs)) | xs <- m''' ]
 					else m'''
 		in m''''
+		
+findChar :: (Eq a) => a -> [[a]] -> (Int, Int)
+findChar x m = head [ a | a <- zip xPosList [0..], fst a /= -1]
+	where
+		xPosList = map (findInLine x) m
+	
+		findInLine :: (Eq a) => a -> [a] -> Int
+		findInLine a r
+			| not (a `elem` r) = -1
+			| otherwise = fst . head $ [ b | b <- zip [0..] r, snd b == a]
